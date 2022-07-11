6237227f6f8a39e1a2426eb1c02e6294ba3ff00b
qosole <williamj99wei@gmail.com>
2022-07-10 19:02:59 -0500

    Add spotify api fetch

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -1,63 +1,52 @@
-// var artistSearch = 'justin';
+var client_id = '17efc8febf1143c5be0fa975fa0836c8';
+var client_secret = '978a363fdfc643da8565312058af76b0';
 
-// var client_id = '17efc8febf1143c5be0fa975fa0836c8';
-// var client_secret = '978a363fdfc643da8565312058af76b0';
+// Access to Spotify's API requires fetching an access token using a base 64 encoded client id and client secret from an app created in Spotify Developers 
+async function authorize() {
+    let myHeaders = new Headers();
+    myHeaders.append("Authorization", 'Basic ' + btoa(client_id + ':' + client_secret)); // Must be base 64 encoded 
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
-// var token = '';
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("grant_type", "client_credentials");
 
-// fetch('https://accounts.spotify.com/api/token', {
-//     method: "POST",
-//     headers: {
-//         'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret),
-//         'Content-Type': "application/x-www-form-urlencoded"
-//     },
-//     body: {
-//         grant_type: 'client_credentials',
-//     },
-//     redirect: 'follow'
-// })
-//     .then(function(response, body) {
-//         if (response.ok) {
-//             token = body.access_token;
-//         } else {
-//             alert('bruh');
-//         }
-//     })
-//     .catch(function(erorr) {
-//         alert('bruh2');
-//     });
+    // The requirements to acquire the access token as specified in Spotify API
+    const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+    }
 
-// // request.post(authOptions, function(error, response, body) {
-// //   if (!error && response.statusCode === 200) {
-// //     token = body.access_token;
-// //   }
-// // });
+    // Fetching the access token
+    let res = await fetch("https://accounts.spotify.com/api/token", requestOptions);
+    res = await res.json();
+    return res.access_token; 
+}
 
-// //var spotifyKey = '17efc8febf1143c5be0fa975fa0836c8:978a363fdfc643da8565312058af76b0';
-// var baseSpotifyURL = 'https://api.spotify.com';
+async function search() {
+    // Calling authorize() to get the access token
+    const access_token = await this.authorize();
+    console.log(access_token);
 
-// function spotifyFetch(event) {
-//   //event.preventDefault();
-  
-//   var URL = baseSpotifyURL + '/v1/search?type=artist&limit=5&market=US&q=' + artistSearch;
-//   fetch(URL, {
-//     headers: {
-//         "Authorization": "Bearer " + token
-//     }
-//   })
-//   .then(function(response) {
-//     if (response.ok) {
-//       console.log(response);
-//       response.json().then(function(data) {
-//         console.log(data);
-//       })
-//     } else {
-//       alert('Error: ' + response.statusText);
-//     }
-//   })
-//   .catch(function(error) {
-//     alert('Unable to retrieve Youtube videos');
-//   });
-// }
+    // Fetching the data from Spotify's daily updating Top 50 Global playlist 
+    playlist_id = '37i9dQZEVXbMDoHDwVN2tF';
+    const BASE_URL = 'https://api.spotify.com';
+    let FETCH_URL = BASE_URL + '/v1/playlists/' + playlist_id;
 
-// spotifyFetch();
+    let myHeaders = new Headers();
+    myHeaders.append("Authorization", 'Bearer ' + access_token);
+    
+    // Using the access token to fetch the data
+    const requestOptions = {
+        method: 'GET',
+        headers: myHeaders
+    }
+
+    // Fetching the playlist data
+    let res = await fetch(FETCH_URL, requestOptions);
+    res = await res.json();
+    console.log(res);
+}
+
+search();
